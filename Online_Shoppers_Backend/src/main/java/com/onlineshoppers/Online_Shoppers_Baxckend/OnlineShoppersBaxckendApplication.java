9a6d77d2003c4419fc7e2d211ff8691de0795598
skawuma <82976186+skawuma@users.noreply.github.com>
2023-06-17 16:38:49 -0400

    Project Setup for Both Back-end And UI

---
+++ Online_Shoppers_Backend/src/main/java/com/onlineshoppers/Online_Shoppers_Baxckend/OnlineShoppersBaxckendApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshoppers.Online_Shoppers_Baxckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppersBaxckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppersBaxckendApplication.class, args);
+	}
+
+}

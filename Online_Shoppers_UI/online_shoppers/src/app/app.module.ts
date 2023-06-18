8767a9037026d4205d42cc93e5aabd4247a54629
skawuma <82976186+skawuma@users.noreply.github.com>
2023-06-18 14:22:57 -0400

    created admin and user components

--- Online_Shoppers_UI/online_shoppers/src/app/app.module.ts
+++ Online_Shoppers_UI/online_shoppers/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ForbiddenComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
